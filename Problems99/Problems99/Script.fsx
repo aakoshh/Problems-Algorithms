25e1c226f7d38f8d8e9fe5c688a36a2199404bb9
Akos Farkas <afarkas@sportingindex.com>
2012-10-30 16:43:41 +0000

    - misc

--- Problems99/Problems99/Script.fsx
+++ Problems99/Problems99/Script.fsx
@@ -1,1 +1,15 @@
-﻿
+﻿#r "FSharp.PowerPack"
+#load "Problems99Lists.fs"
+#load "Problems99Arithmetic.fs"
+#load "Problems99Logic.fs"
+#load "Problems99Trees.fs"
+#load "Problems99Forests.fs"
+#load "Problems99Graphs.fs"
+
+open Problems99.Utils
+open Problems99.Lists
+open Problems99.Arithmetic
+open Problems99.Logic
+open Problems99.Trees
+open Problems99.Forests
+open Problems99.Graphs
