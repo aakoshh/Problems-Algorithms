873a25fabc9c875ff0e25bcb12308452cda377b3
Aa`Koshh <aakoshh@gmail.com>
2013-12-09 15:35:01 +0000

    - fast algorithm for coin changing

--- DeepDives/DeepDives/Algorithms/Coins.fs
+++ DeepDives/DeepDives/Algorithms/Coins.fs
@@ -50,7 +50,7 @@ module Coins =
         change (sum, 0)
 
 
-    /// Change coins by getting rid of the smalles values first.
+    /// Change coins by getting rid of the smalles values greedily.
     /// Can result in inferior solutions like in case of [10; 20; 50] [1; 3; 1] 60
     let changeAndDisposeMostGreedy values pieces sum = 
         // create an array so we can use dynamic programming referring to indexes
@@ -59,8 +59,6 @@ module Coins =
                     |> Array.sort
         let n = coins |> Array.length
 
-        /// Assuming that if we can use a small coin and still finish, 
-        /// then it will never be worse off than not using it.
         let rec change = memoize <| fun (s, i) ->
             if i = n && s <> 0 then
                 None
@@ -77,6 +75,49 @@ module Coins =
                 change (s, i+1)
 
         change (sum, 0)
+
+
+    /// Start adding up the smalles coins and quit as soon as we have our sum.
+    /// This is faster than doing the whole dynamic programming thing.
+    let changeAndDisposeMostBuild values pieces sum = 
+        // using smaller coins first
+        let coins = expandCoins values pieces |> List.sort
+
+        // build up a dictionary of sums as we encounter them, 
+        // thus having the sum built up from the smallest numbers
+        let sums = new System.Collections.Generic.Dictionary<int,int>()
+        sums.[0] <- 0
+
+        // go back and collect the increments we got
+        let rec backtrack s steps =
+            if s = 0 then
+                steps
+            else
+                let prev = sums.[s] 
+                let diff = s - prev
+                backtrack prev (diff :: steps)
+        
+        // take coins one by one and build the sum dict
+        let rec loop lst = 
+            if sums.ContainsKey(sum) then
+                Some(backtrack sum [])
+            else
+                match lst with
+                | hd :: tl -> 
+                    let curr = sums.Keys |> List.ofSeq
+                    for i in curr do
+                        let k = i + hd
+                        if not <| sums.ContainsKey(k) then
+                            sums.[k] <- i
+                    loop tl
+                | [] -> 
+                    None
+
+        match loop coins with
+        | Some(coins) -> Some(coins |> List.length, coins)
+        | None -> None
+
+            
 
 
     module Tests = 
@@ -101,6 +142,17 @@ module Coins =
 
 
         [<Test>]
+        let BuilderMethodIsEquivalentToDynamic() = 
+            let values = [1; 2; 5; 10; 20; 50; 100; 200]
+            let pieces = [160; 138; 172; 146; 38; 25; 180; 107]
+            let value  = 7606
+            let s1 = changeAndDisposeMost values pieces value
+            let s2 = changeAndDisposeMostBuild values pieces value
+            Assert.AreEqual(s1 |> Option.get |> fst, s2 |> Option.get |> fst)
+            Assert.AreEqual(s1 |> Option.get |> snd |> List.sum, s2 |> Option.get |> snd |> List.sum)
+
+
+        [<Test>]
         let AlgorithmFinishesWithin10Seconds() = 
             let values = [1; 2; 5; 10; 20; 50; 100; 200]
             let pieces = [160; 138; 172; 146; 38; 25; 180; 107]
@@ -109,4 +161,16 @@ module Coins =
             let coins  = changeAndDisposeMost values pieces value
             let elapsed = timer.ElapsedMilliseconds
             Assume.That(elapsed < 10000L, sprintf "Should have finished faster than %d ms." elapsed) // not in debug mode
+            printfn "Calculation took %d ms." elapsed 
+
+
+        [<Test>]
+        let BuilderAlgorithmFinishesWithin1Second() = 
+            let values = [1; 2; 5; 10; 20; 50; 100; 200]
+            let pieces = [160; 138; 172; 146; 38; 25; 180; 107]
+            let value  = 7606
+            let timer  = Stopwatch.StartNew()
+            let coins  = changeAndDisposeMostBuild values pieces value
+            let elapsed = timer.ElapsedMilliseconds
+            Assume.That(elapsed < 1000L, sprintf "Should have finished faster than %d ms." elapsed) // not in debug mode
             printfn "Calculation took %d ms." elapsed 
