a542e767becba8a4d411e335b1db99b2d4b94143
Akos Farkas <akos@hellosoda.com>
2015-06-15 00:23:51 +0100

    Monad laws

--- FPinScala/src/test/scala/MonadSuite.scala
+++ FPinScala/src/test/scala/MonadSuite.scala
@@ -5,6 +5,15 @@ import scala.reflect.ClassTag
 
 object MonadProperties extends Properties("Monad") {
 
+  def monadLaws[F[_], A](name: String, m: Monad[F])(implicit ev: Arbitrary[F[A]]) = {
+    property(s"$name map") = forAll { (ma: F[A]) =>
+      m.map(ma)(a => a) == ma
+    }
+  }
+
+  monadLaws[List, Int]("listMonad", Monad.listMonad)
+  monadLaws[Option, String]("optMonad", Monad.optMonad)
+
   property(s"sequence of options") = forAll { (as: List[Option[Int]]) =>
     Monad.optMonad.sequence(as) == (if (as.contains(None)) None else (Some(as.map(_.get))))
   }
